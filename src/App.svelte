900eb09a395eb3e5397fcb1cfe881bc707e0af3e
jamesjulich <51384945+jamesjulich@users.noreply.github.com>
2021-11-17 23:48:00 -0600

    Adds basic functionality and CI/CD.

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,72 @@
 <script>
-	export let name;
+	const regex = /[^\x00-\xFF]/gu;
+
+	let input = "";
+	$: matches = findMatches(input);
+
+	let findMatches = function(text) {
+		matches = [];
+		let lines = text.split("\n");
+		for (let i = 0; i < lines.length; i++) {
+			let found = lines[i].match(regex);
+			//console.log(found);
+			if (found != null && found.length != 0) {
+				matches.push({line: i + 1, values: found});
+			}
+		}
+		return matches;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="container">
+		<h1>Non-ASCII Finder</h1>
+		<textarea placeholder="Paste your text here..." bind:value={input} />
+		{#if matches.length != 0}
+		<h2>Invalid characters: </h2>
+		{#each matches as match}
+			<p>Line {match.line}:
+				{#each match.values as value}
+					{value}&nbsp
+				{/each}
+			</p>
+		{/each}
+	{/if}
+	</div>
 </main>
+<footer>
+	<p>Made by James Julich - <a href="https://github.com/jamesjulich/nonascii.github.io">Github</a></p>
+</footer>
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		margin: auto;
+		width: 80vw;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	footer {
+		width: 80vw;
+		margin: auto;
+		width: 80vw;
+		padding-bottom: 30px;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.container {
+		width: 100%;
+		height: 100%;
+		margin-bottom: 20px;
+	}
+
+	textarea {
+		width: 100%;
+		height: 70vh;
+		line-height: 1.5em;
+		white-space: nowrap;
+	}
+
+	p {
+		word-wrap: break-word;
+		line-height: 1.5em;
 	}
 </style>
